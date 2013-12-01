a197b07eabe8616f74214894e78c7740d0ced8d3
Maximilian Fellner <max.fellner@gmail.com>
2013-12-01 01:04:16 +0100

    Added link to source code.

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(name='app',
       description='BMUK SAP frontend',
       author='Maximilian Fellner',
       author_email='max.fellner@gmail.com',
-      url='http://maximilianfellner.eu',
+      url='https://github.com/mfellner/bmuksap',
       install_requires=['Flask>=0.10'], )
